98e7d82f12cabf0fadc6676b3d3eeac0fac55d47
naoyamada-fastlabel <nao.yamada@fastlabel.ai>
2026-04-17 19:33:58 +0900

    軽微な修正をした。

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-export OPT_NAME="<last name>_<first name>" # example: "yamada_taro"
+export OPT_NAME="kosuke_umino" # example: "yamada_taro"
 export REPO_ID="operator-training/$OPT_NAME" # Don't Change!!
-export TASK_NAME="<TASK_NAME>" # Set task name
-export REPO_ROOT="$HOME/.cache/huggingfac e/lerobot/$REPO_ID". # Don't Change!!
-export FRONT_CAM_ID="<front cam id>"  # Set front cam id
-export LEFT_CAM_ID="<left cam id>"  # Set left cam id
-export RIGHT_CAM_ID="<right cam id>"  # Set right cam id
+export TASK_NAME="Pick up the cube and place it on the tray" # Set task name
+export REPO_ROOT="$HOME/.cache/huggingface/lerobot/$REPO_ID" # Don't Change!!
+export FRONT_CAM_ID="8"  # Set front cam id
+export LEFT_CAM_ID="0"  # Set left cam id
+export RIGHT_CAM_ID="4"  # Set right cam id
+export EPISODE_TIME="60"
+export REST_TIME="20"
